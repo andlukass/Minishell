fb6a041a969005bff2022712355e6706b26e3a22
andlukass <lopess.andrade@gmail.com>
2023-12-08 15:57:35 +0000

    bug fix in ft_env

--- src/builtins/ft_env.c
+++ src/builtins/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: llopes-d <llopes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 16:58:58 by llopes-d          #+#    #+#             */
-/*   Updated: 2023/12/08 14:43:09 by llopes-d         ###   ########.fr       */
+/*   Updated: 2023/12/08 15:57:19 by llopes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ void	ft_env(void)
 	current = get_data()->env;
 	while(current)
 	{
-		printf("%s=%s\n",current->key, current->value);
+		if(current->key)
+			printf("%s=%s\n",current->key, current->value);
 		current = current->next;
 	}
 }
