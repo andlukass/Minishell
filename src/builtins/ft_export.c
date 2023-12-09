9ea25216d8e5970d8b1387fb378c8e9079ceb8fa
andlukass <lopess.andrade@gmail.com>
2023-12-09 21:41:07 +0000

    implement ft_export

--- src/builtins/ft_export.c
+++ src/builtins/ft_export.c
@@ -3,34 +3,119 @@
 /*                                                        :::      ::::::::   */
 /*   ft_export.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llopes-d <llopes-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 15:20:27 by llopes-d          #+#    #+#             */
-/*   Updated: 2023/12/08 16:16:02 by llopes-d         ###   ########.fr       */
+/*   Updated: 2023/12/09 21:40:24 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-//sem args: deve mostrar todo o env 
-//com 1 arg: se receber uma string sem '=' adiciona ao env
-//com 2+ args: so aceita o segundo arg se o arg for o nome de alguma var ja existente ou o nome da que esta a ser criada
+static t_env	*get_copy()
+{
+	t_env	*copy;
+	t_env	*current;
+	char	*variable;
+
+	current = get_data()->env;
+	copy = NULL;
+	while(current)
+	{
+		variable = strdup(current->variable);
+		add_next_node(&copy, create_new_value(variable));
+		current = current->next;
+	}
+	return (copy);
+}
 
 static void	print_export()
 {
-	
+	t_env	*current;
+	t_env	*begin;
+	char	*temp;
+	int		compare_size;
+
+	current = get_copy();
+	begin = current;
+	while(current->next)
+	{
+		compare_size = strchr(current->variable, '=') - current->variable;
+		if(strncmp(current->variable, current->next->variable, compare_size) > 0)
+		{
+			temp = current->variable;
+			current->variable = current->next->variable;
+			current->next->variable = temp;
+			current = begin;
+		}
+		current = current->next;
+	}
+	current = begin;
+	while(current)
+	{
+		if (strchr(current->variable, '=') && !(*(strchr(current->variable, '=') + 1)))
+			printf("declare -x %s''\n", current->variable);
+		else
+			printf("declare -x %s\n", current->variable);
+		current = current->next;
+	}
+	free_env(begin);
+}
+
+static void	add_to_env(char *argument)
+{
+	t_env	*current;
+	char	*variable;
+	char	*old_key;
+	char	*key;
+
+	if (*argument >= '0' && *argument <= '9')
+	{
+		printf("'%c' identifier can't start with numbers\n", *argument);
+		return;
+	}
+	variable = strdup(argument);
+	if (strchr(variable, '='))
+		key = strndup(variable, strchr(variable, '=') - variable);
+	else
+		key = strndup(variable, ft_strlen(variable));
+	current = get_data()->env;
+	while(current)
+	{
+		if (strchr(current->variable, '='))
+			old_key = strndup(current->variable, strchr(current->variable, '=') - current->variable);
+		else
+			old_key = strndup(current->variable, ft_strlen(current->variable));
+		if (!strcmp(old_key, key))
+		{
+			free(old_key);
+			free(key);
+			if (!strchr(variable, '='))
+			{
+				free(variable);
+				return;
+			}
+			free(current->variable);
+			current->variable = variable;
+			return ;
+		}
+		free(old_key);
+		current = current->next;
+	}
+	free(key);
+	add_next_node(&get_data()->env, create_new_value(variable));
 }
 
 void	ft_export(void)
 {
 	t_env *current;
+	int	index;
 
+	index = 1;
 	current = get_data()->env;
 	if (!get_data()->input_array[1])
-	while(current)
-	{
-		if(current->key)
-			printf("%s=%s\n",current->key, current->value);
-		current = current->next;
-	}
+		print_export();
+	else
+		while(get_data()->input_array[index])
+			add_to_env(get_data()->input_array[index++]);
 }
