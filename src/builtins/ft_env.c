ee237b05a32960074c10c63c5b1b744fb5cc471c
andlukass <lopess.andrade@gmail.com>
2023-12-10 19:59:58 +0000

    add arg verification

--- src/builtins/ft_env.c
+++ src/builtins/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: llopes-d <llopes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 16:58:58 by llopes-d          #+#    #+#             */
-/*   Updated: 2023/12/10 14:54:51 by llopes-d         ###   ########.fr       */
+/*   Updated: 2023/12/10 19:21:50 by llopes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ void	ft_env(void)
 {
 	t_env	*current;
 
+	if (get_data()->input_array[1])
+	{
+		printf("env doesn't take any arguments!\n");
+		return ;
+	}
 	current = get_data()->env;
 	while (current)
 	{
