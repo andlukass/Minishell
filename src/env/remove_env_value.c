493f0c868ef911bb70bbc6d85b373ad095e5784f
andlukass <lopess.andrade@gmail.com>
2023-12-11 00:09:06 +0000

    add (rm & update) functions

--- src/env/remove_env_value.c
+++ src/env/remove_env_value.c
@@ -1,37 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   update_env_value.c                                    :+:      :+:    :+:   */
+/*   remove_env_value.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 20:21:22 by llopes-d          #+#    #+#             */
-/*   Updated: 2023/12/10 22:22:53 by user             ###   ########.fr       */
+/*   Updated: 2023/12/10 23:58:58 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	update_env_value(char *key, char *variable)
+void	remove_env_value(char *key)
 {
 	t_env	*current;
+	t_env	*previous;
 	char	*env_key;
 
 	current = get_data()->env;
-	if (get_data()->env)
+	previous = NULL;
+	while (current)
 	{
-		while (current->next)
+		env_key = get_env_key(current->variable);
+		if(!strcmp(key, env_key))
 		{
-			env_key = get_env_key(current->variable);
-			if (!strcmp(env_key, key))
-			{
-				free(env_key);
-				free(current->variable);
-				current->variable = variable;
-				return ;
-			}
+			if (!previous)
+				get_data()->env = current->next;
+			else
+				previous->next = current->next;
+			free(current->variable);
+			free(current);
 			free(env_key);
-			current = current->next;
+			return ;
 		}
+		free(env_key);
+		previous = current;
+		current = current->next;
 	}
 }
