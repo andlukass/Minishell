7379f8a621490f7530c52a1ab1b99089146f28f1
andlukass <lopess.andrade@gmail.com>
2023-12-11 23:27:30 +0000

    fix ft_pwd

--- src/builtins/ft_pwd.c
+++ src/builtins/ft_pwd.c
@@ -15,9 +15,9 @@
 void	ft_pwd(void)
 {
 	if (get_data()->input_array[1] != NULL)
-	{
 		printf("pwd doesn't take any arguments!\n");
-		return ;
-	}
-	printf("%s\n", get_env_value("PWD"));
+	else if (get_env_value("PWD"))
+		printf("%s\n", get_env_value("PWD"));
+	else
+		printf("PWD is not set!\n");
 }
