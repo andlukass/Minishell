0036768a05ef943bf9fb8f4b0d48a20d694ecdad
andlukass <lopess.andrade@gmail.com>
2024-01-07 19:55:01 +0000

    fix get_last_redirection

--- src/parser/get_last_redirection.c
+++ src/parser/get_last_redirection.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_last_redirection.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isbraz-d <isbraz-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: llopes-d <llopes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 15:04:04 by isbraz-d          #+#    #+#             */
-/*   Updated: 2024/01/07 14:38:15 by isbraz-d         ###   ########.fr       */
+/*   Updated: 2024/01/07 19:54:04 by llopes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,21 +40,20 @@ static int	find_reds(char *command, int index, char c)
 char	*get_last_redirection(char *command, char c)
 {
 	char	*reds;
-	int		index;
 	int		i;
 	int		j;
 
 	reds = malloc(sizeof(char) * 3);
 	if (!reds)
 		return (NULL);
-	i = ft_strlen(command) - 1;
+	j = ft_strlen(command) - 1;
+	i = find_reds(command, j, c);
 	j = 0;
-	index = find_reds(command, i, c);
-	if (index != -1)
+	if (i != -1)
 	{
-		reds[j++] = command[index];
-		if (command[index - 1] == '>' || command[index - 1] == '<')
-			reds[j++] = command[index - 1];
+		reds[j++] = command[i];
+		if (i > 0 && (command[i - 1] == '>' || command[i - 1] == '<'))
+			reds[j++] = command[i - 1];
 		reds[j] = '\0';
 		if (ft_strlen(reds) == 1 || ft_strlen(reds) == 2)
 			return (reds);
