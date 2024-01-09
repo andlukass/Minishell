46a35b1c37c25be11e09428678aeaaf2c42b60ff
Ismael Braz <ismael.braz03@hotmail.com>
2024-01-09 17:38:06 +0000

    starting expander

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: isbraz-d <isbraz-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 16:59:14 by user              #+#    #+#             */
-/*   Updated: 2024/01/07 15:45:03 by isbraz-d         ###   ########.fr       */
+/*   Updated: 2024/01/09 15:56:09 by isbraz-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,6 +89,8 @@ int		ft_strncmp(const char *s1, const char *s2, unsigned int n);
  * 0 if they are all equal
  */
 int		ft_strcmp(const char *s1, const char *s2);
+
+int	ft_isalpha(int c);
 
 /* --------------------------------------- */
 
