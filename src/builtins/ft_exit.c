a94845a1f96fda983482ac9ddcf6500908784ad7
andlukass <lopess.andrade@gmail.com>
2024-01-17 18:52:12 +0000

    exit fixed

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -3,27 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llopes-d <llopes-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 16:24:50 by llopes-d          #+#    #+#             */
-/*   Updated: 2024/01/16 16:39:13 by llopes-d         ###   ########.fr       */
+/*   Updated: 2024/01/17 18:49:59 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	ft_exit(char **command, int status)
+static int is_str_numeric(char *str)
 {
-	if (command)
+	int	index;
+
+	index = 0;
+	if (!str)
+		return (1);
+	if (str[index] == '-' || str[index] == '+')
+		index++;
+	while(str[index])
 	{
-		if (command[1] != (void *)0)
-		{
-			printf("exit doesn't take any arguments!\n");
-			get_data()->exit_status = 1;
-			return ;
-		}
-		printf("exit\n");
+		if (str[index] < '0' || str[index] > '9')
+			return (0);
+		index++;
 	}
+	return (1);
+}
+
+static int	check_arguments(char **command, long *status)
+{
+	if (!command)
+		return (1);
+	if (ft_strlen(command[1]) > 19 || !is_str_numeric(command[1]))
+	{
+		get_data()->exit_status = 512;
+		printf("exit: numeric argument required\n");
+		return (0);
+	}
+	else if (command[1] && command[2])
+	{
+		get_data()->exit_status = 256;
+		printf("exit: too many arguments\n");
+		return (0);
+	}
+	else
+	{
+		if (command[1])
+			*status = atoi(command[1]);
+		else
+			*status = 0;
+		printf("exit\n");
+		return (1);
+	}
+}
+
+void	ft_exit(char **command, long status)
+{
+	int	must_exit;
+	
+	must_exit = check_arguments(command, &status);
+	if (!must_exit)
+		return ;
 	free_env(get_data()->env);
 	if (get_data()->commands)
 		free_commands(get_data()->commands);
