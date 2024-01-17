97cffb1049ccec63ac3e3fd67a2268ecf58c96a4
andlukass <lopess.andrade@gmail.com>
2024-01-17 15:15:53 +0000

    echo fixed

--- src/builtins/ft_echo.c
+++ src/builtins/ft_echo.c
@@ -3,38 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llopes-d <llopes-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 16:58:58 by llopes-d          #+#    #+#             */
-/*   Updated: 2024/01/16 16:09:37 by llopes-d         ###   ########.fr       */
+/*   Updated: 2024/01/17 15:15:38 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+static int	have_flag(char *str)
+{
+	int	index;
+
+	index = 1;
+	if (!str || str[0] != '-')
+		return (0);
+	while(str[index])
+	{
+		if (str[index] != 'n')
+			return (0);
+		index++;
+	}
+	return (1);
+}
 
 void	ft_echo(char **command)
 {
 	int	i;
 	int	has_flag;
 
-	i = 1;
 	has_flag = 0;
-	if (command[1])
+	i = 1;
+	if (have_flag(command[1]) && ++has_flag)
+		i = 2;
+	while (command[i])
 	{
-		if (command[1][0] == '-' && command[1][1] != ' ' && ++i && ++has_flag)
-		{
-			if (ft_strcmp(&command[1][1], "n"))
-			{
-				printf("echo has only the '-n' flag\n");
-				ft_exit(NULL, 1);
-			}
-		}
-		while (command[i])
-		{
-			printf("%s", command[i++]);
-			if (command[i])
-				printf(" ");
-		}
+		printf("%s", command[i++]);
+		if (command[i])
+			printf(" ");
 	}
 	if (!has_flag)
 		printf("\n");
